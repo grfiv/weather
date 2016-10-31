52e35709e9449565ff383e1d049416a1e641e4a1
George Fisher <george@georgefisher.com>
2016-10-31 16:14:30 -0400

    finished part 2, adding a PHP to process the API call

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ###Retrieve real-time weather data    
 
 * HTML5, CSS3
-* AJAX
+* AJAX / PHP
 * jQuery
 * jQuery Mobile
 * jQuery Template
@@ -15,4 +15,8 @@ Citations:
 
 * [Build a Mobile Web App Using jQuery Mobile & AJAX](https://www.youtube.com/playlist?list=PLWyjPWK51nBVANRhi0077JKVs2iMf9HA8)   
 * [Dark Sky developer API](https://darksky.net/dev/)   
-* [ Alessio Atzeni: Meteocons Weather Icons](http://www.alessioatzeni.com/meteocons/)
+* [ Alessio Atzeni: Meteocons Weather Icons](http://www.alessioatzeni.com/meteocons/)   
+* [JSONview for Chrome](https://chrome.google.com/webstore/detail/jsonview/chklaanhfefbnpoihckbnefhakgolnmc)   
+* [Download a URL's Content Using PHP cURL](https://davidwalsh.name/curl-download)   
+* [Using JQuery AJAX and php](https://openenergymonitor.org/emon/node/107)   
+* [Get Latitude and Longitude](http://www.latlong.net/)   
---
+++ weather_api.php
@@ -0,0 +1,70 @@
+<?php
+/** @file
+ *
+ * Make an api call to Dark Sky on behalf of
+ * my little jQuery Mobile app
+ */
+
+# get lat/lon if request properly formatted; else send error
+# ==========================================================
+if (isset($_GET['lat']) &&  isset($_GET['lon'])) {
+    $lat = (float) $_GET['lat'];
+    $lon = (float) $_GET['lon'];
+} else {
+    header('Content-type: application/json');
+    $json_msg = array();
+    $json_msg['status']  = 'error';
+    $json_msg['error']   = 'request incorrectly formatted';
+    $json_msg['message'] = "correct format is weather_api.php?lat=37.8267&lon=-122.4233";
+    echo json_encode($json_msg);
+    exit;
+}
+
+# retrieve my api-keyed url from an undisclosed location
+# ======================================================
+include("inc/dark-sky-api-call.php");
+
+# see where the request is coming from
+# ====================================
+$remote_ip_parts    = explode (".", $_SERVER['REMOTE_ADDR']);
+$localhost_ip_parts = explode (".", LOCALHOST);
+$isp_ip_parts       = explode (".", MY_ISP);
+
+if (($remote_ip_parts[0] != $localhost_ip_parts[0]) &&
+    ($remote_ip_parts[0] != $isp_ip_parts[0])) exit;
+
+if ($remote_ip_parts[1] != $localhost_ip_parts[1] &&
+    $remote_ip_parts[1] != $isp_ip_parts[1]) exit;
+
+# build out the api call url
+# ==========================
+$url = DARK_SKY_API_CALL . $lat . "," . $lon;
+
+/**
+ * Download a URL’s Content Using PHP cURL
+ *
+ * citation: https://davidwalsh.name/curl-download
+ *
+ * @param {string} the api url
+ *
+ * @return {string} the api response in JSON format
+ */
+function get_data($url) {
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+$returned_content = get_data($url);
+
+# pass along the JSON to whoever called us
+# ----------------------------------------
+header('Content-type: application/json');
+echo $returned_content;
+
+?>
+
